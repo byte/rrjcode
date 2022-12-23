67c217e5e21fe6a44e6d880f433285a9c1a9be58
Colin Charles <ccharles@gmail.com>
2022-12-24 06:37:15 +0800

    fix weird wordpress characters

---
+++ wordpress-weirdcharacters.sql
@@ -0,0 +1,19 @@
+-- wordpress getting you down with weird characters after a MySQL upgrade? this fixes it
+
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬Å“', 'â€œ');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬Â', 'â€');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬â„¢', 'â€™');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬Ëœ', 'â€˜');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬â€', 'â€“');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬â€œ', 'â€”');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬Â¢', '-');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬Â¦', 'â€¦');
+
+UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬Å“', 'â€œ');
+UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬Â', 'â€');
+UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬â„¢', 'â€™');
+UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬Ëœ', 'â€˜');
+UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬â€', 'â€“');
+UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬â€œ', 'â€”');
+UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬Â¢', '-');
+UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬Â¦', 'â€¦');
