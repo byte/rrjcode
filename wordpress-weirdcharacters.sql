3788f7c68077645f9632b107d85539ea10d6efa4
Colin Charles <ccharles@gmail.com>
2023-11-22 23:45:10 +0800

    one more pesky character

--- wordpress-weirdcharacters.sql
+++ wordpress-weirdcharacters.sql
@@ -8,6 +8,7 @@ UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬â€', '
 UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬â€œ', 'â€”');
 UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬Â¢', '-');
 UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬Â¦', 'â€¦');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Â', '');
 
 UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬Å“', 'â€œ');
 UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬Â', 'â€');
