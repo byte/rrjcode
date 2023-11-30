334a78ab49b6f4f74aa0bda0669bdf857aca5d0d
Colin Charles <ccharles@gmail.com>
2023-11-30 23:45:52 +0800

    more fixes

--- wordpress-weirdcharacters.sql
+++ wordpress-weirdcharacters.sql
@@ -10,6 +10,9 @@ UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬Â¢', '-')
 UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢â‚¬Â¦', 'â€¦');
 UPDATE wp_posts SET post_content = REPLACE(post_content, 'Â', '');
 UPDATE wp_posts SET post_content = REPLACE(post_content, 'ï»¿', '');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã©', 'é');
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'Ã¢', 'â');
+
 
 UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬Å“', 'â€œ');
 UPDATE wp_comments SET comment_content = REPLACE(comment_content, 'Ã¢â‚¬Â', 'â€');
